7324512460400b762e7213c079dd6f742b1d5940
chirag wankhede <chirag20wankhede@gmail.com>
2024-09-06 14:58:22 +0530

    Live Api Fetching

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,31 +1,54 @@
 import Rescard from "./Rescard";
 import resObj from "../utils/mockdata";
+import Shimmer from "./Shimmer";
 import { useEffect, useState } from "react";
 
 const Body = () => {
-const [restuarantlist , setRestuarantlist] = useState(resObj)
+  const [restuarantlist, setRestuarantlist] = useState([]);
+
+  const[button , setButton]=useState("Login")
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const jsonData = await data.json();
+
+    setRestuarantlist(jsonData?.data.cards[1]?.card.card?.gridElements?.infoWithStyle?.restaurants);
+    
+
+  };
 
 
 
-return (
-  <div className="body">
-    <div className="top">
-      <button className="top-btn" 
-      onClick={()=>{
-        const filterList = restuarantlist.filter((resdata)=>resdata.info.avgRating> 4.5);
-        setRestuarantlist(filterList);
-        }}> Top Rated Restaurant</button>
+
+  return restuarantlist.length === 0 ? <Shimmer/> : (
+    <div className="body">
+      <div className="top">
+        <button
+          className="top-btn"
+          onClick={() => {
+            const filterList = restuarantlist.filter(
+              (resdata) => resdata.info.avgRating > 4.3
+            );
+            setRestuarantlist(filterList);
+          }}>Top Rated Restaurant</button>
+
+        <button className="login-btn" onClick={()=>{
+          button === "Login"? setButton("Logout") : setButton("Login")}}>{button}</button>
+      </div>
+      <div className="res-container">
+        {restuarantlist.map((resdata) => (
+          <Rescard key={resdata.info.id} resdata={resdata} />
+        ))}
+      </div>
     </div>
-    <div className="res-container">
-      {restuarantlist.map((resdata) => (
-        <Rescard key={resdata.info.id} resdata={resdata} />
-      ))}
-    </div>
-  </div>
-);
+  );
 };
 
-
 export default Body;
-
-
---
+++ src/components/Shimmer.js
@@ -0,0 +1,23 @@
+
+
+const Shimmer = () => {
+  return (
+    <div className="shimmer-boc">
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+        <div className="shimmer"></div>
+
+    </div>
+  )
+}
+
+export default Shimmer
